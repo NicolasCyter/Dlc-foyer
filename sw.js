38aba482a97095bb861368897dc0f35a64b69223
NicolasCyter <nicolas.cyter@yahoo.fr>
2026-08-25 21:21:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Cache minimal : l'app reste utilisable hors ligne une fois ouverte.
 // Le nom du cache change à chaque version pour forcer le rafraîchissement.
-const CACHE = 'dlc-v14';
+const CACHE = 'dlc-v15';
 const FICHIERS = ['./', './index.html', './manifest.json', './icone.png', './icone-180.png'];
 
 self.addEventListener('install', e => {
